7b621de9adc454fca790709ea47de3f64b5fd93c
BENGTSON <BENGTSON@MOLINA-PC>
2022-11-13 21:19:51 -0300

    Agregar archivos de proyecto.

---
+++ CapaEntidades/Paciente.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Paciente
+    {
+        #region Atributos
+        private int HClinica;
+        private string Nombre;
+        private string Apellido;
+        private string Direccion;
+        private int Edad;
+        private float Altura;
+        private float Peso;
+        private bool Lentes;
+        private float IMC;
+        #endregion
+
+        #region Propiedades
+        public int PHClinica
+        {
+            set { HClinica = value; }
+            get { return HClinica; }
+        }
+        public int PEdad
+        {
+            set { Edad = value; }
+            get { return Edad; }
+        }
+        public float PAltura
+        {
+            set { Altura = value; }
+            get { return Altura; }
+        }
+        public float PPeso
+        {
+            set { Peso = value; }
+            get { return Peso; }
+        }
+        public string PNombre
+        {
+            set { Nombre = value; }
+            get { return Nombre; }
+        }
+        public string PApellido
+        {
+            set { Apellido = value; }
+            get { return Apellido; }
+        }
+        public string PDireccion
+        {
+            set { Direccion = value; }
+            get { return Direccion; }
+        }
+        public bool PLentes
+        {
+            set { Lentes = value; }
+            get { return Lentes; }
+        }
+        public float PIMC
+        {
+            get { return IMC; }
+        }
+        #endregion
+
+        #region Constructores
+        public Paciente()
+        { }
+
+        public Paciente(int HCln, string Nomb, string Apll, string Dir, int Edd, float Alt, float Pes, bool Len)
+        {
+            HClinica = HCln;
+            Nombre = Nomb;
+            Apellido = Apll;
+            Direccion = Dir;
+            Edad = Edd;
+            Altura = Alt;
+            Peso = Pes;
+            Lentes = Len;
+            IMC = ClaculoIMC(Pes, Alt);
+
+        }
+        #endregion
+
+        #region Metodos
+        protected float ClaculoIMC(float Pes, float Alt)
+        {
+            float imc;
+            imc = Pes / (Alt*Alt);
+            return imc;
+        }
+        #endregion
+    }
+}
---
+++ CapaPresentacion/AltaPaciente.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace CapaPresentacion
+{
+    partial class AltaPaciente
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TxbEdad = new System.Windows.Forms.TextBox();
+            this.TbxAlt = new System.Windows.Forms.TextBox();
+            this.BtnAlta = new System.Windows.Forms.Button();
+            this.RbtLentNo = new System.Windows.Forms.RadioButton();
+            this.RbtLentSi = new System.Windows.Forms.RadioButton();
+            this.TxbHCln = new System.Windows.Forms.TextBox();
+            this.LblHCln = new System.Windows.Forms.Label();
+            this.LblLent = new System.Windows.Forms.Label();
+            this.LblPeso = new System.Windows.Forms.Label();
+            this.LblAlt = new System.Windows.Forms.Label();
+            this.LblEdad = new System.Windows.Forms.Label();
+            this.LblDir = new System.Windows.Forms.Label();
+            this.LblApll = new System.Windows.Forms.Label();
+            this.LblNomb = new System.Windows.Forms.Label();
+            this.TbxPeso = new System.Windows.Forms.TextBox();
+            this.TbxDir = new System.Windows.Forms.TextBox();
+            this.TbxApll = new System.Windows.Forms.TextBox();
+            this.TbxNomb = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 217);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(726, 162);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // TxbEdad
+            // 
+            this.TxbEdad.Location = new System.Drawing.Point(70, 102);
+            this.TxbEdad.Name = "TxbEdad";
+            this.TxbEdad.Size = new System.Drawing.Size(119, 20);
+            this.TxbEdad.TabIndex = 48;
+            // 
+            // TbxAlt
+            // 
+            this.TbxAlt.Location = new System.Drawing.Point(266, 103);
+            this.TbxAlt.Name = "TbxAlt";
+            this.TbxAlt.Size = new System.Drawing.Size(119, 20);
+            this.TbxAlt.TabIndex = 47;
+            // 
+            // BtnAlta
+            // 
+            this.BtnAlta.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAlta.Location = new System.Drawing.Point(663, 133);
+            this.BtnAlta.Name = "BtnAlta";
+            this.BtnAlta.Size = new System.Drawing.Size(75, 69);
+            this.BtnAlta.TabIndex = 46;
+            this.BtnAlta.Text = "ALTA";
+            this.BtnAlta.UseVisualStyleBackColor = true;
+            // 
+            // RbtLentNo
+            // 
+            this.RbtLentNo.AutoSize = true;
+            this.RbtLentNo.Location = new System.Drawing.Point(699, 103);
+            this.RbtLentNo.Name = "RbtLentNo";
+            this.RbtLentNo.Size = new System.Drawing.Size(39, 17);
+            this.RbtLentNo.TabIndex = 45;
+            this.RbtLentNo.TabStop = true;
+            this.RbtLentNo.Text = "No";
+            this.RbtLentNo.UseVisualStyleBackColor = true;
+            // 
+            // RbtLentSi
+            // 
+            this.RbtLentSi.AutoSize = true;
+            this.RbtLentSi.Location = new System.Drawing.Point(638, 103);
+            this.RbtLentSi.Name = "RbtLentSi";
+            this.RbtLentSi.Size = new System.Drawing.Size(34, 17);
+            this.RbtLentSi.TabIndex = 44;
+            this.RbtLentSi.TabStop = true;
+            this.RbtLentSi.Text = "Si";
+            this.RbtLentSi.UseVisualStyleBackColor = true;
+            // 
+            // TxbHCln
+            // 
+            this.TxbHCln.Location = new System.Drawing.Point(638, 12);
+            this.TxbHCln.Name = "TxbHCln";
+            this.TxbHCln.Size = new System.Drawing.Size(100, 20);
+            this.TxbHCln.TabIndex = 43;
+            // 
+            // LblHCln
+            // 
+            this.LblHCln.AutoSize = true;
+            this.LblHCln.Location = new System.Drawing.Point(554, 15);
+            this.LblHCln.Name = "LblHCln";
+            this.LblHCln.Size = new System.Drawing.Size(78, 13);
+            this.LblHCln.TabIndex = 42;
+            this.LblHCln.Text = "Historia Clínica";
+            // 
+            // LblLent
+            // 
+            this.LblLent.AutoSize = true;
+            this.LblLent.Location = new System.Drawing.Point(583, 105);
+            this.LblLent.Name = "LblLent";
+            this.LblLent.Size = new System.Drawing.Size(39, 13);
+            this.LblLent.TabIndex = 39;
+            this.LblLent.Text = "Lentes";
+            // 
+            // LblPeso
+            // 
+            this.LblPeso.AutoSize = true;
+            this.LblPeso.Location = new System.Drawing.Point(403, 107);
+            this.LblPeso.Name = "LblPeso";
+            this.LblPeso.Size = new System.Drawing.Size(52, 13);
+            this.LblPeso.TabIndex = 38;
+            this.LblPeso.Text = "Peso (kg)";
+            // 
+            // LblAlt
+            // 
+            this.LblAlt.AutoSize = true;
+            this.LblAlt.Location = new System.Drawing.Point(211, 107);
+            this.LblAlt.Name = "LblAlt";
+            this.LblAlt.Size = new System.Drawing.Size(54, 13);
+            this.LblAlt.TabIndex = 37;
+            this.LblAlt.Text = "Altura (mt)";
+            // 
+            // LblEdad
+            // 
+            this.LblEdad.AutoSize = true;
+            this.LblEdad.Location = new System.Drawing.Point(15, 105);
+            this.LblEdad.Name = "LblEdad";
+            this.LblEdad.Size = new System.Drawing.Size(32, 13);
+            this.LblEdad.TabIndex = 36;
+            this.LblEdad.Text = "Edad";
+            // 
+            // LblDir
+            // 
+            this.LblDir.AutoSize = true;
+            this.LblDir.Location = new System.Drawing.Point(15, 75);
+            this.LblDir.Name = "LblDir";
+            this.LblDir.Size = new System.Drawing.Size(52, 13);
+            this.LblDir.TabIndex = 35;
+            this.LblDir.Text = "Dirección";
+            // 
+            // LblApll
+            // 
+            this.LblApll.AutoSize = true;
+            this.LblApll.Location = new System.Drawing.Point(394, 45);
+            this.LblApll.Name = "LblApll";
+            this.LblApll.Size = new System.Drawing.Size(44, 13);
+            this.LblApll.TabIndex = 34;
+            this.LblApll.Text = "Apellido";
+            // 
+            // LblNomb
+            // 
+            this.LblNomb.AutoSize = true;
+            this.LblNomb.Location = new System.Drawing.Point(15, 45);
+            this.LblNomb.Name = "LblNomb";
+            this.LblNomb.Size = new System.Drawing.Size(44, 13);
+            this.LblNomb.TabIndex = 33;
+            this.LblNomb.Text = "Nombre";
+            // 
+            // TbxPeso
+            // 
+            this.TbxPeso.Location = new System.Drawing.Point(458, 103);
+            this.TbxPeso.Name = "TbxPeso";
+            this.TbxPeso.Size = new System.Drawing.Size(119, 20);
+            this.TbxPeso.TabIndex = 30;
+            // 
+            // TbxDir
+            // 
+            this.TbxDir.Location = new System.Drawing.Point(70, 72);
+            this.TbxDir.Name = "TbxDir";
+            this.TbxDir.Size = new System.Drawing.Size(668, 20);
+            this.TbxDir.TabIndex = 29;
+            // 
+            // TbxApll
+            // 
+            this.TbxApll.Location = new System.Drawing.Point(444, 42);
+            this.TbxApll.Name = "TbxApll";
+            this.TbxApll.Size = new System.Drawing.Size(294, 20);
+            this.TbxApll.TabIndex = 28;
+            // 
+            // TbxNomb
+            // 
+            this.TbxNomb.Location = new System.Drawing.Point(70, 42);
+            this.TbxNomb.Name = "TbxNomb";
+            this.TbxNomb.Size = new System.Drawing.Size(294, 20);
+            this.TbxNomb.TabIndex = 27;
+            // 
+            // AltaPaciente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 391);
+            this.Controls.Add(this.TxbEdad);
+            this.Controls.Add(this.TbxAlt);
+            this.Controls.Add(this.BtnAlta);
+            this.Controls.Add(this.RbtLentNo);
+            this.Controls.Add(this.RbtLentSi);
+            this.Controls.Add(this.TxbHCln);
+            this.Controls.Add(this.LblHCln);
+            this.Controls.Add(this.LblLent);
+            this.Controls.Add(this.LblPeso);
+            this.Controls.Add(this.LblAlt);
+            this.Controls.Add(this.LblEdad);
+            this.Controls.Add(this.LblDir);
+            this.Controls.Add(this.LblApll);
+            this.Controls.Add(this.LblNomb);
+            this.Controls.Add(this.TbxPeso);
+            this.Controls.Add(this.TbxDir);
+            this.Controls.Add(this.TbxApll);
+            this.Controls.Add(this.TbxNomb);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "AltaPaciente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alta Paciente";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox TxbEdad;
+        private System.Windows.Forms.TextBox TbxAlt;
+        private System.Windows.Forms.Button BtnAlta;
+        private System.Windows.Forms.RadioButton RbtLentNo;
+        private System.Windows.Forms.RadioButton RbtLentSi;
+        private System.Windows.Forms.TextBox TxbHCln;
+        private System.Windows.Forms.Label LblHCln;
+        private System.Windows.Forms.Label LblLent;
+        private System.Windows.Forms.Label LblPeso;
+        private System.Windows.Forms.Label LblAlt;
+        private System.Windows.Forms.Label LblEdad;
+        private System.Windows.Forms.Label LblDir;
+        private System.Windows.Forms.Label LblApll;
+        private System.Windows.Forms.Label LblNomb;
+        private System.Windows.Forms.TextBox TbxPeso;
+        private System.Windows.Forms.TextBox TbxDir;
+        private System.Windows.Forms.TextBox TbxApll;
+        private System.Windows.Forms.TextBox TbxNomb;
+    }
+}
+
